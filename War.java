59ca6a2c9dcba8a651bdc0cab566da00fcffebdd
Daniel Desmond <df.dezmond9@gmail.com>
2021-12-19 17:26:33 -0500

    created method to compare two cards

--- War.java
+++ War.java
@@ -2,4 +2,15 @@ public class War {
     public static void main(String[] args) {
 
     }
+
+    private String compareCards(Card card1, Card card2) {
+        // returns true if card1 is greater than card2
+        if (card1.getValue() > card2.getValue()) {
+            return "card1";
+        } else if (card1.getValue() < card2.getValue()) {
+            return "card2";
+        } else {
+            return "war";
+        }
+    }
 }
