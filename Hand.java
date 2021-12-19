80d49fa3641a070852b234cdcfc8c8f989459316
Daniel Desmond <df.dezmond9@gmail.com>
2021-12-18 19:30:24 -0500

    adjusted Hand to work better in real situation

--- Hand.java
+++ Hand.java
@@ -1,6 +1,19 @@
 import java.util.ArrayList;
 
 public class Hand {
+    private ArrayList<Card> cards;
+
+    public Hand() {
+    }
+
+    public Card getNextCard() {
+        return this.cards.remove(0);
+    }
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+
     static void main(String[] args) {
 
     }
