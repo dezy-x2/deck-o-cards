e4813bcae597430a369e5a2075bee4d4fd4f5c94
Daniel Desmond <df.dezmond9@gmail.com>
2021-12-20 22:06:53 -0500

    created the logic in War to run a game

--- War.java
+++ War.java
@@ -4,15 +4,37 @@ public class War {
     public static void main(String[] args) {
         Hand jacksHand = new Hand("Jack");
         Hand johnsHand = new Hand("John");
-
+        Deck deck = new Deck();
+        deck.deal(new ArrayList<Hand>() {
+            {
+                add(jacksHand);
+                add(johnsHand);
+            }
+        });
+        while (getWinner(jacksHand, johnsHand) == null) {
+            Card jacksPlayCard = jacksHand.getNextCard(1).get(0);
+            Card johnsPlayCard = johnsHand.getNextCard(1).get(0);
+            String roundResults = compareCards(jacksPlayCard, johnsPlayCard);
+            if (roundResults.equals("card1")) {
+                jacksHand.addCard(johnsPlayCard);
+            } else if (roundResults.equals("card2")) {
+                johnsHand.addCard(jacksPlayCard);
+            } else if (roundResults.equals("war")) {
+                handleWar(jacksHand, johnsHand);
+            }
+        }
+        Hand finalWinner = getWinner(jacksHand, johnsHand);
+        if (finalWinner != null) {
+            System.out.println(finalWinner.name + " wins!");
+        }
     }
 
-    private void handleWar(Hand hand1, Hand hand2) {
+    private static void handleWar(Hand hand1, Hand hand2) {
         ArrayList<Card> cards1ForGrabs = hand1.getNextCard(4);
         ArrayList<Card> cards2ForGrabs = hand2.getNextCard(4);
-        Card warCard1 = cards1ForGrabs.remove(0);
-        Card warCard2 = cards2ForGrabs.remove(0);
-        String warResult = this.compareCards(warCard1, warCard2);
+        Card warCard1 = cards1ForGrabs.remove(0);// ! somekind of error around here with it not catching that there
+        Card warCard2 = cards2ForGrabs.remove(0);// ! aren't enough cards
+        String warResult = compareCards(warCard1, warCard2);
         if (warResult.equals("card1")) {
             hand1.addCard(cards1ForGrabs); // TODO: this still needs work since we don't have a way to handle war again
             hand1.addCard(cards2ForGrabs);
@@ -25,17 +47,17 @@ public class War {
         }
     }
 
-    private Hand getWinner(Hand hand1, Hand hand2) {
-        if (hand1.getSize() == 0) {
+    private static Hand getWinner(Hand hand1, Hand hand2) {
+        if (hand1.getSize() == 0 || hand1.getSize() == 1) {
             return hand2;
-        } else if (hand2.getSize() == 0) {
+        } else if (hand2.getSize() == 0 || hand2.getSize() == 1) {
             return hand1;
         } else {
             return null;
         }
     }
 
-    private String compareCards(Card card1, Card card2) {
+    private static String compareCards(Card card1, Card card2) {
         // returns true if card1 is greater than card2
         if (card1.getValue() > card2.getValue()) {
             return "card1";
